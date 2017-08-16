e10fa4d7a0e19e4c81071865e17082e2032ecbd2
r57zone <r57zone@gmail.com>
2017-08-16 14:49:01 +0400

    Updated C++ sample

--- С++/GetData/GetData/GetData.cpp
+++ С++/GetData/GetData/GetData.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include <Windows.h>
+#include <atlbase.h>
 
 typedef struct _HMDData
 {
@@ -18,19 +19,39 @@ typedef DWORD(_stdcall *_GetHMDData)(__out THMD* myHMD);
 
 int main()
 {
-	//Path to "TOVR.dll" on Registry - "HKEY_CURRENT_USER\Software\TrueOpenVR" parameter "Library"
-	HMODULE hDll = LoadLibrary(L"TOVR.dll");
+	//Read main library path from registry
+	CRegKey key;
+	TCHAR libPath[MAX_PATH];
+	
+	LONG status = key.Open(HKEY_CURRENT_USER, _T("Software\\TrueOpenVR"));
+	if (status == ERROR_SUCCESS)
+	{
+		ULONG libPathSize = sizeof(libPath);
+		status = key.QueryStringValue(_T("Library"), libPath, &libPathSize);
+
+		if (status != ERROR_SUCCESS)
+		{
+			printf("Error read library path\n");
+			return 1;
+		}
+	}
+	else
+	{
+		printf("Error open registry path TrueOpenVR");
+		return 1;
+	}
+	key.Close();
+
+	//Load main library
+	HMODULE hDll = LoadLibrary(libPath);
 	if (hDll == NULL) {
 		printf("ERROR: unable to load DLL\n");
 		return 1;
 	}
 
 	THMD myHMD;
-
 	ZeroMemory(&myHMD, sizeof(THMD));
-
 	_GetHMDData GetHMDData;
-
 	GetHMDData = (_GetHMDData)GetProcAddress(hDll, "GetHMDData");
 
 	if (GetHMDData == NULL) {
@@ -39,17 +60,15 @@ int main()
 	}
 
 	DWORD iResult = GetHMDData(&myHMD);
-
 	Sleep(300);
-
 	GetHMDData(&myHMD);
 
 	printf("Result = %d\n", iResult);
-
 	printf("X = %f\nY = %f\nZ = %f\nYaw = %f\nPitch = %f\nRoll = %f\n\n", myHMD.X, myHMD.Y, myHMD.Z, myHMD.Yaw, myHMD.Pitch, myHMD.Roll);
 
-	system("pause");
 	FreeLibrary(hDll);
 	hDll = nullptr;
+
+	system("pause");
 	return 0;
 }
